48cf958e89649426d946b5b63b038bc315f703d5
Alex Han <ahan98@gmail.com>
2023-03-16 03:45:04 -0700

    refactor

--- lua/luasnip-latex-snippets/math_iA.lua
+++ lua/luasnip-latex-snippets/math_iA.lua
@@ -1,174 +1,175 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local f = ls.function_node
+local parse = ls.parser.parse_snippet
 local with_priority = require("luasnip-latex-snippets.util.utils").with_priority
 
 local math_iA = {
-  s(
-    {
-      trig = "(%a)bar",
-      wordTrig = false,
-      regTrig = true,
-      name = "bar",
-      priority = 100,
-    },
-    f(function(_, snip)
-      return string.format("\\overline{%s}", snip.captures[1])
-    end, {})
-  ),
-  s(
-    {
-      trig = "(%a)hat",
-      wordTrig = false,
-      regTrig = true,
-      name = "hat",
-      priority = 100,
-    },
-    f(function(_, snip)
-      return string.format("\\hat{%s}", snip.captures[1])
-    end, {})
-  ),
+	s(
+		{
+			trig = "(%a)bar",
+			wordTrig = false,
+			regTrig = true,
+			name = "bar",
+			priority = 100,
+		},
+		f(function(_, snip)
+			return string.format("\\overline{%s}", snip.captures[1])
+		end, {})
+	),
+	s(
+		{
+			trig = "(%a)hat",
+			wordTrig = false,
+			regTrig = true,
+			name = "hat",
+			priority = 100,
+		},
+		f(function(_, snip)
+			return string.format("\\hat{%s}", snip.captures[1])
+		end, {})
+	),
 
-  -- BEGIN (AHAN98)
-  ls.parser.parse_snippet({ trig = "_", name = "underscore" }, "_{$1}$0 "),
-  ls.parser.parse_snippet({ trig = "nin", name = "not in" }, "\\not\\in "),
-  ls.parser.parse_snippet({ trig = "sbeq", name = "subseteq" }, "\\subseteq "),
-  ls.parser.parse_snippet({ trig = "speq", name = "supseteq" }, "\\supseteq "),
+	-- BEGIN (AHAN98)
+	parse({ trig = "_", name = "underscore" }, "_{$1}$0 "),
+	parse({ trig = "nin", name = "not in" }, "\\not\\in "),
+	parse({ trig = "sbeq", name = "subseteq" }, "\\subseteq "),
+	parse({ trig = "speq", name = "supseteq" }, "\\supseteq "),
 
-  ls.parser.parse_snippet({ trig = "Cup", name = "bigcup" }, "\\bigcup_{${1:i \\in ${2: I}}} $0"),
-  ls.parser.parse_snippet({ trig = "cup", name = "cup" }, "\\cup_{${1:i \\in ${2: I}}} $0"),
+	parse({ trig = "Cup", name = "bigcup" }, "\\bigcup_{${1:i \\in ${2: I}}} $0"),
+	parse({ trig = "cup", name = "cup" }, "\\cup_{${1:i \\in ${2: I}}} $0"),
 
-  ls.parser.parse_snippet({ trig = "Cap", name = "bigcap" }, "\\bigcap_{${1:i \\in ${2: I}}} $0"),
-  ls.parser.parse_snippet({ trig = "cap", name = "cap" }, "\\cap_{${1:i \\in ${2: I}}} $0"),
+	parse({ trig = "Cap", name = "bigcap" }, "\\bigcap_{${1:i \\in ${2: I}}} $0"),
+	parse({ trig = "cap", name = "cap" }, "\\cap_{${1:i \\in ${2: I}}} $0"),
 
-  ls.parser.parse_snippet({ trig = "fim", name = "infimum" }, "\\inf_{$1}$0 "),
-  ls.parser.parse_snippet({ trig = "sup", name = "supremum" }, "\\sup_{$1}$0 "),
+	parse({ trig = "fim", name = "infimum" }, "\\inf_{$1}$0 "),
+	parse({ trig = "sup", name = "supremum" }, "\\sup_{$1}$0 "),
 
-  with_priority(
-    ls.parser.parse_snippet({ trig = "amin", name = "argmin" }, "\\argmin_{$1}$0 "),
-    1100
-  ),
-  with_priority(
-    ls.parser.parse_snippet({ trig = "amax", name = "argmax" }, "\\argmax_{$1}$0 "),
-    1100
-  ),
+	with_priority(
+		parse({ trig = "amin", name = "argmin" }, "\\argmin_{$1}$0 "),
+		1100
+	),
+	with_priority(
+		parse({ trig = "amax", name = "argmax" }, "\\argmax_{$1}$0 "),
+		1100
+	),
 
-  with_priority(ls.parser.parse_snippet({ trig = "min", name = "min" }, "\\min_{$1}$0 "), 100),
-  with_priority(ls.parser.parse_snippet({ trig = "max", name = "max" }, "\\max_{$1}$0 "), 100),
+	parse({ trig = "min", name = "min" }, "\\min_{$1}$0 ")
+	parse({ trig = "max", name = "max" }, "\\max_{$1}$0 ")
 
-  ls.parser.parse_snippet(
-    { trig = ":=", name = "colon equals (lhs defined as rhs)" },
-    "\\coloneqq "
-  ),
-  ls.parser.parse_snippet(
-    { trig = "=:", name = "equals colon (rhs defined as lhs)" },
-    "\\eqqcolon "
-  ),
-  -- END
+	parse(
+		{ trig = ":=", name = "colon equals (lhs defined as rhs)" },
+		"\\coloneqq "
+	),
+	parse(
+		{ trig = "=:", name = "equals colon (rhs defined as lhs)" },
+		"\\eqqcolon "
+	),
+	-- END
 
-  ls.parser.parse_snippet({ trig = "td", name = "to the ... power ^{}" }, "^{$1}$0 "),
-  ls.parser.parse_snippet({ trig = "rd", name = "to the ... power ^{()}" }, "^{($1)}$0 "),
-  ls.parser.parse_snippet({ trig = "cb", name = "Cube ^3" }, "^3 "),
-  ls.parser.parse_snippet({ trig = "sr", name = "Square ^2" }, "^2"),
+	parse({ trig = "td", name = "to the ... power ^{}" }, "^{$1}$0 "),
+	parse({ trig = "rd", name = "to the ... power ^{()}" }, "^{($1)}$0 "),
+	parse({ trig = "cb", name = "Cube ^3" }, "^3 "),
+	parse({ trig = "sr", name = "Square ^2" }, "^2"),
 
-  ls.parser.parse_snippet({ trig = "EE", name = "exists" }, "\\exists "),
-  ls.parser.parse_snippet({ trig = "AA", name = "forall" }, "\\forall "),
-  ls.parser.parse_snippet({ trig = "xnn", name = "xn" }, "x_{n}"),
-  ls.parser.parse_snippet({ trig = "ynn", name = "yn" }, "y_{n}"),
-  ls.parser.parse_snippet({ trig = "xii", name = "xi" }, "x_{i}"),
-  ls.parser.parse_snippet({ trig = "yii", name = "yi" }, "y_{i}"),
-  ls.parser.parse_snippet({ trig = "xjj", name = "xj" }, "x_{j}"),
-  ls.parser.parse_snippet({ trig = "yjj", name = "yj" }, "y_{j}"),
-  ls.parser.parse_snippet({ trig = "xp1", name = "x" }, "x_{n+1}"),
-  ls.parser.parse_snippet({ trig = "xmm", name = "x" }, "x_{m}"),
-  ls.parser.parse_snippet({ trig = "R0+", name = "R0+" }, "\\mathbb{R}_0^+"),
+	parse({ trig = "EE", name = "exists" }, "\\exists "),
+	parse({ trig = "AA", name = "forall" }, "\\forall "),
+	parse({ trig = "xnn", name = "xn" }, "x_{n}"),
+	parse({ trig = "ynn", name = "yn" }, "y_{n}"),
+	parse({ trig = "xii", name = "xi" }, "x_{i}"),
+	parse({ trig = "yii", name = "yi" }, "y_{i}"),
+	parse({ trig = "xjj", name = "xj" }, "x_{j}"),
+	parse({ trig = "yjj", name = "yj" }, "y_{j}"),
+	parse({ trig = "xp1", name = "x" }, "x_{n+1}"),
+	parse({ trig = "xmm", name = "x" }, "x_{m}"),
+	parse({ trig = "R0+", name = "R0+" }, "\\mathbb{R}_0^+"),
 
-  -- ls.parser.parse_snippet({ trig = "notin", name = "not in " }, "\\not\\in "),
-  --
-  -- ls.parser.parse_snippet({ trig = "cc", name = "subset" }, "\\subset "),
+	-- parse({ trig = "notin", name = "not in " }, "\\not\\in "),
+	--
+	-- parse({ trig = "cc", name = "subset" }, "\\subset "),
 
-  with_priority(
-    ls.parser.parse_snippet({ trig = "<->", name = "leftrightarrow" }, "\\leftrightarrow"),
-    200
-  ),
-  with_priority(ls.parser.parse_snippet({ trig = "...", name = "ldots" }, "\\ldots "), 100),
-  ls.parser.parse_snippet({ trig = "!>", name = "mapsto" }, "\\mapsto "),
-  ls.parser.parse_snippet({ trig = "iff", name = "iff" }, "\\iff"),
-  ls.parser.parse_snippet({ trig = "ooo", name = "\\infty" }, "\\infty"),
-  ls.parser.parse_snippet(
-    { trig = "rij", name = "mrij" },
-    "(${1:x}_${2:n})_{${3:$2}\\in${4:\\N}}$0"
-  ),
-  ls.parser.parse_snippet({ trig = "nabl", name = "nabla" }, "\\nabla "),
-  ls.parser.parse_snippet({ trig = "<!", name = "normal" }, "\\triangleleft "),
-  ls.parser.parse_snippet(
-    { trig = "floor", name = "floor" },
-    "\\left\\lfloor $1 \\right\\rfloor$0"
-  ),
-  ls.parser.parse_snippet({ trig = "mcal", name = "mathcal" }, "\\mathcal{$1}$0"),
-  ls.parser.parse_snippet({ trig = "//", name = "Fraction" }, "\\frac{$1}{$2}$0"),
-  ls.parser.parse_snippet({ trig = "\\\\\\", name = "setminus" }, "\\setminus"),
-  with_priority(ls.parser.parse_snippet({ trig = "->", name = "to" }, "\\to "), 100),
+	with_priority(
+		parse({ trig = "<->", name = "leftrightarrow" }, "\\leftrightarrow"),
+		200
+	),
+	with_priority(parse({ trig = "...", name = "ldots" }, "\\ldots "), 100),
+	parse({ trig = "!>", name = "mapsto" }, "\\mapsto "),
+	parse({ trig = "iff", name = "iff" }, "\\iff"),
+	parse({ trig = "ooo", name = "\\infty" }, "\\infty"),
+	parse(
+		{ trig = "rij", name = "mrij" },
+		"(${1:x}_${2:n})_{${3:$2}\\in${4:\\N}}$0"
+	),
+	parse({ trig = "nabl", name = "nabla" }, "\\nabla "),
+	parse({ trig = "<!", name = "normal" }, "\\triangleleft "),
+	parse(
+		{ trig = "floor", name = "floor" },
+		"\\left\\lfloor $1 \\right\\rfloor$0"
+	),
+	parse({ trig = "mcal", name = "mathcal" }, "\\mathcal{$1}$0"),
+	parse({ trig = "//", name = "Fraction" }, "\\frac{$1}{$2}$0"),
+	parse({ trig = "\\\\\\", name = "setminus" }, "\\setminus"),
+	with_priority(parse({ trig = "->", name = "to" }, "\\to "), 100),
 
-  ls.parser.parse_snippet(
-    { trig = "letw", name = "let omega" },
-    "Let $\\Omega \\subset \\C$ be open."
-  ),
-  -- ls.parser.parse_snippet({ trig = "nnn", name = "bigcap" }, "\\bigcap_{${1:i \\in ${2: I}}} $0"),
-  ls.parser.parse_snippet({ trig = "norm", name = "norm" }, "\\|$1\\|$0"),
-  ls.parser.parse_snippet({ trig = "<>", name = "hokje" }, "\\diamond "),
-  ls.parser.parse_snippet({ trig = ">>", name = ">>" }, "\\gg"),
-  ls.parser.parse_snippet({ trig = "<<", name = "<<" }, "\\ll"),
+	parse(
+		{ trig = "letw", name = "let omega" },
+		"Let $\\Omega \\subset \\C$ be open."
+	),
+	-- parse({ trig = "nnn", name = "bigcap" }, "\\bigcap_{${1:i \\in ${2: I}}} $0"),
+	parse({ trig = "norm", name = "norm" }, "\\|$1\\|$0"),
+	parse({ trig = "<>", name = "hokje" }, "\\diamond "),
+	parse({ trig = ">>", name = ">>" }, "\\gg"),
+	parse({ trig = "<<", name = "<<" }, "\\ll"),
 
-  ls.parser.parse_snippet({ trig = "stt", name = "text subscript" }, "_\\text{$1} $0"),
-  ls.parser.parse_snippet({ trig = "tt", name = "text" }, "\\text{$1}$0"),
+	parse({ trig = "stt", name = "text subscript" }, "_\\text{$1} $0"),
+	parse({ trig = "tt", name = "text" }, "\\text{$1}$0"),
 
-  ls.parser.parse_snippet({ trig = "xx", name = "cross" }, "\\times "),
+	parse({ trig = "xx", name = "cross" }, "\\times "),
 
-  with_priority(ls.parser.parse_snippet({ trig = "**", name = "cdot" }, "\\cdot "), 100),
+	with_priority(parse({ trig = "**", name = "cdot" }, "\\cdot "), 100),
 
-  ls.parser.parse_snippet(
-    { trig = "cvec", name = "column vector" },
-    "\\begin{pmatrix} ${1:x}_${2:1}\\\\ \\vdots\\\\ $1_${2:n} \\end{pmatrix}"
-  ),
-  ls.parser.parse_snippet({ trig = "ceil", name = "ceil" }, "\\left\\lceil $1 \\right\\rceil $0"),
-  ls.parser.parse_snippet({ trig = "OO", name = "emptyset" }, "\\O"),
-  ls.parser.parse_snippet({ trig = "RR", name = "R" }, "\\mathbb{R}"),
-  ls.parser.parse_snippet({ trig = "QQ", name = "Q" }, "\\mathbb{Q}"),
-  ls.parser.parse_snippet({ trig = "ZZ", name = "Z" }, "\\mathbb{Z}"),
-  -- ls.parser.parse_snippet({ trig = "UU", name = "cup" }, "\\cup "),
-  ls.parser.parse_snippet({ trig = "NN", name = "n" }, "\\mathbb{N}"),
-  ls.parser.parse_snippet({ trig = "||", name = "mid" }, " \\mid "),
-  -- ls.parser.parse_snippet({ trig = "Nn", name = "cap" }, "\\cap "),
-  ls.parser.parse_snippet(
-    { trig = "bmat", name = "bmat" },
-    "\\begin{bmatrix} $1 \\end{bmatrix} $0"
-  ),
-  -- ls.parser.parse_snippet({ trig = "uuu", name = "bigcup" }, "\\bigcup_{${1:i \\in ${2: I}}} $0"),
-  ls.parser.parse_snippet({ trig = "DD", name = "D" }, "\\mathbb{D}"),
-  ls.parser.parse_snippet({ trig = "HH", name = "H" }, "\\mathbb{H}"),
-  ls.parser.parse_snippet({ trig = "lll", name = "l" }, "\\ell"),
-  with_priority(
-    ls.parser.parse_snippet(
-      { trig = "dint", name = "integral" },
-      "\\int_{${1:-\\infty}}^{${2:\\infty}} ${3:${TM_SELECTED_TEXT}} $0"
-    ),
-    300
-  ),
+	parse(
+		{ trig = "cvec", name = "column vector" },
+		"\\begin{pmatrix} ${1:x}_${2:1}\\\\ \\vdots\\\\ $1_${2:n} \\end{pmatrix}"
+	),
+	parse({ trig = "ceil", name = "ceil" }, "\\left\\lceil $1 \\right\\rceil $0"),
+	parse({ trig = "OO", name = "emptyset" }, "\\O"),
+	parse({ trig = "RR", name = "R" }, "\\mathbb{R}"),
+	parse({ trig = "QQ", name = "Q" }, "\\mathbb{Q}"),
+	parse({ trig = "ZZ", name = "Z" }, "\\mathbb{Z}"),
+	-- parse({ trig = "UU", name = "cup" }, "\\cup "),
+	parse({ trig = "NN", name = "n" }, "\\mathbb{N}"),
+	parse({ trig = "||", name = "mid" }, " \\mid "),
+	-- parse({ trig = "Nn", name = "cap" }, "\\cap "),
+	parse(
+		{ trig = "bmat", name = "bmat" },
+		"\\begin{bmatrix} $1 \\end{bmatrix} $0"
+	),
+	-- parse({ trig = "uuu", name = "bigcup" }, "\\bigcup_{${1:i \\in ${2: I}}} $0"),
+	parse({ trig = "DD", name = "D" }, "\\mathbb{D}"),
+	parse({ trig = "HH", name = "H" }, "\\mathbb{H}"),
+	parse({ trig = "lll", name = "l" }, "\\ell"),
+	with_priority(
+		parse(
+			{ trig = "dint", name = "integral" },
+			"\\int_{${1:-\\infty}}^{${2:\\infty}} ${3:${TM_SELECTED_TEXT}} $0"
+		),
+		300
+	),
 
-  ls.parser.parse_snippet({ trig = "==", name = "equals" }, [[&= $1 \\\\]]),
-  ls.parser.parse_snippet({ trig = "!=", name = "not equals" }, "\\neq "),
-  ls.parser.parse_snippet({ trig = "compl", name = "complement" }, "^{c}"),
-  ls.parser.parse_snippet({ trig = "__", name = "subscript" }, "_{$1}$0"),
-  ls.parser.parse_snippet({ trig = "=>", name = "implies" }, "\\implies"),
-  ls.parser.parse_snippet({ trig = "=<", name = "implied by" }, "\\impliedby"),
-  ls.parser.parse_snippet({ trig = "<<", name = "<<" }, "\\ll"),
+	parse({ trig = "==", name = "equals" }, [[&= $1 \\\\]]),
+	parse({ trig = "!=", name = "not equals" }, "\\neq "),
+	parse({ trig = "compl", name = "complement" }, "^{c}"),
+	parse({ trig = "__", name = "subscript" }, "_{$1}$0"),
+	parse({ trig = "=>", name = "implies" }, "\\implies"),
+	parse({ trig = "=<", name = "implied by" }, "\\impliedby"),
+	parse({ trig = "<<", name = "<<" }, "\\ll"),
 
-  ls.parser.parse_snippet({ trig = "<=", name = "leq" }, "\\le "),
-  ls.parser.parse_snippet({ trig = ">=", name = "geq" }, "\\ge "),
-  ls.parser.parse_snippet({ trig = "invs", name = "inverse" }, "^{-1}"),
-  ls.parser.parse_snippet({ trig = "~~", name = "~" }, "\\sim "),
-  ls.parser.parse_snippet({ trig = "conj", name = "conjugate" }, "\\overline{$1}$0"),
+	parse({ trig = "<=", name = "leq" }, "\\le "),
+	parse({ trig = ">=", name = "geq" }, "\\ge "),
+	parse({ trig = "invs", name = "inverse" }, "^{-1}"),
+	parse({ trig = "~~", name = "~" }, "\\sim "),
+	parse({ trig = "conj", name = "conjugate" }, "\\overline{$1}$0"),
 }
 
 return math_iA
